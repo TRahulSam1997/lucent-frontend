bdc9c94542064da12b07f91b765d88cb62cd0b77
Rahul <rahul.samaranayake@zendesk.com>
2022-12-18 20:23:52 +1100

    Ghost cms API endpoint exposed

--- lib/ghost/posts.ts
+++ lib/ghost/posts.ts
@@ -6,15 +6,23 @@ import GhostContentAPI from "@tryghost/content-api";
 //     };
 //   };
   
-const ghostURL: string = process.env.GHOST_URL || ''
-const ghostKey: string = process.env.GHOST_KEY || ''
-const ghostVersion: string = process.env.GHOST_VERSION || 'v5.0'
+const ghostURL: string = process.env.GHOST_URL || '';
+const ghostKey: string = process.env.GHOST_KEY || '';
+const ghostVersion: string = process.env.GHOST_VERSION || 'v5.0';
 
 const api = new GhostContentAPI({
     url: ghostURL,
     key: ghostKey,
     // @ts-expect-error
     version: ghostVersion
-})
+});
 
-export {}
+export async function getPosts() {
+  return await api.posts
+    .browse({
+      limit: 'all'
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
