e662bc2b747e77f76cf3be31b778f741f8259bd0
Rahul <rahul.samaranayake@zendesk.com>
2022-12-23 12:15:29 +1100

    author api function added

---
+++ pages/authors/[name].tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const AuthorPage = (props:any) => {
+  return (
+    <div> AuthorPage</div>
+  )
+}
+
+// export async getStaticPaths() => {
+    
+// }
+
+export default AuthorPage
