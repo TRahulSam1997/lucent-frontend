20ae11d07b70879bdd4381c6a46b2575a632ec45
Rahul <rahul.samaranayake@zendesk.com>
2023-02-16 20:40:52 +1300

    build section updates for the right panel

--- components/rightPanel.tsx
+++ components/rightPanel.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { BuildSection } from './sections/buildSection'
 
 export const RightPanel = () => {
   return (
     <div>
         <h1>Right Panel</h1>
+        <BuildSection />
     </div>
   )
 }
---
+++ components/sections/buildSection.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const BuildSection = () => {
+  return (
+    <div>
+        <div>
+            <h1>Build</h1>
+        </div>
+        <div>
+            <p>Icon</p>
+            <h3>Collaborate with experienced engineers and designers.</h3>
+            <p>Share your ideas, prototypes, MVPs, and code bases safely to receive live feedback and suggestions with the opportunity to pair-program.</p>
+        </div>
+    </div>
+  )
+}
