d31bfdc2ebcb5c9f717c19ccfd39d2ff846619c2
Rahul <rahul.samaranayake@zendesk.com>
2023-02-16 16:54:49 +1300

    Nav bar added

---
+++ components/nav.tsx
@@ -0,0 +1,16 @@
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Lucent</a></li>
+        <li><a href="#">Salon</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Resources</a></li>
+        <li><a href="#">Sign In</a></li>
+        <li><a href="#">Get started </a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
