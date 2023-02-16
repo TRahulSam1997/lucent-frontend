189df9b38820ccea8fd15487d57c65c101448d71
Rahul <rahul.samaranayake@zendesk.com>
2023-02-16 21:20:11 +1300

    Connect section updates for the right panel

--- components/rightPanel.tsx
+++ components/rightPanel.tsx
@@ -6,8 +6,10 @@ export const RightPanel = () => {
   return (
     <div>
         <h1>Right Panel</h1>
-        <div style={{ backgroundColor: '#2F3047' }}>
-          <BuildSection />
+        <div>
+          <div style={{ display: 'inline-block', backgroundColor: '#2F3047' }}>
+            <BuildSection />
+          </div>
           <ConnectSection />
         </div>
     </div>
