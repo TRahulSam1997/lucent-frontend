182a40af656b8ebdc41a761b9619649fa15ee505
Rahul <rahul.samaranayake@zendesk.com>
2023-02-16 21:24:34 +1300

    find a CTO section added

--- components/sections/findACTOSection.tsx
+++ components/sections/findACTOSection.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const FindACTOSection = () => {
   return (
-    <div>FindACTOSection</div>
+    <div>
+        <img 
+            src="" 
+            alt="Slack workspace" 
+            title="Source: Slack (https://Slack.com)"
+        />
+        <h1>
+            FindACTOSection
+        </h1>
+    </div>
   )
 }
